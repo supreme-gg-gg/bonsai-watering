29ea5ec4a89f10560c4305ac5bdb8a298d024b0c
Jet Chiang <jetjiang.ez@gmail.com>
2025-03-18 00:56:23 -0400

    remove old app

--- mobile-ios/bonsai-client/bonsai-client/BluetoothService.swift
+++ mobile-ios/bonsai-client/bonsai-client/BluetoothService.swift
@@ -5,79 +5,94 @@
 //  Created by Jet Chiang on 2025-03-16.
 //
 
-import Foundation
 import CoreBluetooth
 
 class BLEPeripheralManager: NSObject, CBPeripheralManagerDelegate {
 	private var peripheralManager: CBPeripheralManager!
 	private var moistureCharacteristic: CBMutableCharacteristic!
+	private let serviceUUID = CBUUID(string: "12345678-1234-5678-1234-56789abcdef3")
+	private let characteristicUUID = CBUUID(string: "12345678-1234-5678-1234-56789abcdef4")
 	private var messageUpdateHandler: ((String) -> Void)?
+	private var serviceSetupCompleted = false
 
 	override init() {
 		super.init()
-		peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+		let queue = DispatchQueue(label: "com.bonsai.bluetooth", qos: .userInitiated)
+		peripheralManager = CBPeripheralManager(delegate: self, queue: queue)
 	}
 
 	func startAdvertising(messageHandler: @escaping (String) -> Void) {
-		messageUpdateHandler = messageHandler
-		guard peripheralManager.state == .poweredOn else {
-			print("Bluetooth not ready, waiting for state update...")
-			return
+		self.messageUpdateHandler = messageHandler
+		if peripheralManager.state == .poweredOn {
+			setupServiceIfNeeded()
 		}
-		setupService()
 	}
 
 	func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
 		if peripheral.state == .poweredOn {
+			setupServiceIfNeeded()
+		}
+	}
+
+	private func setupServiceIfNeeded() {
+		if !serviceSetupCompleted {
 			setupService()
-		} else {
-			print("❌ Bluetooth not available")
+			serviceSetupCompleted = true
 		}
 	}
 
 	private func setupService() {
-		let serviceUUID = CBUUID(string: "12345678-1234-5678-1234-56789abcdef0")
-		let characteristicUUID = CBUUID(string: "12345678-1234-5678-1234-56789abcdef1")
-
+		print("Setting up Service and Char")
+		print("Service UUID: \(serviceUUID.uuidString)")
+		print("Char UUID: \(characteristicUUID.uuidString)")
 		moistureCharacteristic = CBMutableCharacteristic(
 			type: characteristicUUID,
 			properties: [.read, .write, .notify],
 			value: nil,
 			permissions: [.readable, .writeable]
 		)
-
 		let sensorService = CBMutableService(type: serviceUUID, primary: true)
 		sensorService.characteristics = [moistureCharacteristic]
-
-		print("✅ Adding service...")
-		peripheralManager.add(sensorService) // Will trigger didAdd callback
+		peripheralManager.add(sensorService)
 	}
 
 	func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
-		if let error = error {
-			print("Failed to add service: \(error.localizedDescription)")
-			return
-		}
-		print("Service added successfully")
-
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // Delay to ensure service is registered
-			self.peripheralManager.startAdvertising([
-				CBAdvertisementDataLocalNameKey: "BonsaiPeripheral",
+		if error == nil {
+			let advertisementData: [String: Any] = [
+				CBAdvertisementDataLocalNameKey: "BonsaiPeripheral_Test",
 				CBAdvertisementDataServiceUUIDsKey: [service.uuid]
-			])
-			print("Started advertising BLE service")
+			]
+			peripheralManager.startAdvertising(advertisementData)
+		}
+	}
+
+	func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
+		if request.characteristic.uuid == characteristicUUID {
+			request.value = moistureCharacteristic.value ?? "No data".data(using: .utf8)
+			peripheral.respond(to: request, withResult: .success)
+		} else {
+			peripheral.respond(to: request, withResult: .attributeNotFound)
 		}
 	}
 
 	func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
 		for request in requests {
-			if let value = request.value, let message = String(data: value, encoding: .utf8) {
+			if request.characteristic.uuid == characteristicUUID, let value = request.value, let message = String(data: value, encoding: .utf8) {
+				moistureCharacteristic.value = value
+				peripheral.updateValue(value, for: moistureCharacteristic, onSubscribedCentrals: nil)
+				peripheral.respond(to: request, withResult: .success)
 				DispatchQueue.main.async {
 					self.messageUpdateHandler?(message)
 				}
-				moistureCharacteristic.value = value
+			} else {
+				peripheral.respond(to: request, withResult: .attributeNotFound)
 			}
-			peripheral.respond(to: request, withResult: .success)
+		}
+	}
+
+	func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+		if characteristic.uuid == characteristicUUID {
+			peripheral.updateValue("Initial data".data(using: .utf8)!, for: moistureCharacteristic, onSubscribedCentrals: nil)
 		}
 	}
 }
