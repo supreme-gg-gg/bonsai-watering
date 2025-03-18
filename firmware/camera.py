ee5060382acfa9373d73b225cd0ebb81ab7ad183
Jet Chiang <jetjiang.ez@gmail.com>
2025-03-18 19:29:58 -0400

    new camera code

--- firmware/camera.py
+++ firmware/camera.py
@@ -1,18 +1,11 @@
 from picamera2 import Picamera2, Preview
 import time
-
 picam2 = Picamera2()
-
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-
-picam2.configure(preview_config)
-
-picam2.start_preview(Preview.QTGL)
-
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+# if using GUI:
+# picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.DRM)
 picam2.start()
 time.sleep(2)
-
-metadata = picam2.capture_file("test.jpg")
-print(metadata)
-
-picam2.close()
+picam2.capture_file("test.jpg")
