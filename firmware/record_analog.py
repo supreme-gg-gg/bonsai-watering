8c8ef658914d01a8dbe690875fef540523376546
Jet Chiang <jetjiang.ez@gmail.com>
2025-04-02 22:16:57 -0400

    read analog signal

--- firmware/record_analog.py
+++ firmware/record_analog.py
@@ -11,30 +11,37 @@ spi.max_speed_hz=1000000
 # Function to read SPI data from MCP3008 chip
 # Channel must be an integer 0-7
 def ReadChannel(channel):
-  adc = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((adc[1]&3) << 8) + adc[2]
-  return data
-  
-     
+    adc = spi.xfer2([1,(8+channel)<<4,0])
+    data = ((adc[1]&3) << 8) + adc[2]
+    return data
+
+
 def ConverttoPercent(data):
-  percent = int(round(data/10.24))
-  return percent
-  
+    percent = int(round(data/10.24))
+    return percent
+
 # Define sensor channel (on MCP3008)
 moisture_channel  = 0
 
 # Define delay between readings
 delay = 2
 
-while True:
+try:
+    while True:
+        # Read the moisture sensor data
+        moisture_level = ReadChannel(moisture_channel)
+        moisture_percent = ConverttoPercent(moisture_level)
 
-  # Read the moisture sensor data
-  moisture_level = ReadChannel(moisture_channel)
-  moisture_percent = ConverttoPercent(moisture_level)
+        # Print out results
+        print("--------------------------------------------")
+        print("Moisture : {} ({}%)".format(moisture_level,moisture_percent))  
 
-  # Print out results
-  print("--------------------------------------------")
-  print("Moisture : {} ({}%)".format(moisture_level,moisture_percent))  
-      
-  # Wait before repeating loop
-  time.sleep(delay)
+        # Wait before repeating loop
+        time.sleep(delay)
+except KeyboardInterrupt:
+    print("Program stopped by user")
+finally:
+    # Cleanup GPIO settings
+    GPIO.cleanup()
+    spi.close()
+    print("GPIO cleaned up and SPI closed")
